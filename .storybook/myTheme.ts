ccb61382a95ef4cf28f65563c7b9c6930e008e89
Diego Navarro <code@diegognt.dev>
2023-10-15 17:02:37 -0500

    feat: adds Storybook for component showcase

---
+++ .storybook/myTheme.ts
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'My React Component Library',
+  brandUrl: 'https://components.diegognt.dev',
+  brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
+  brandTarget: '_self',
+});
+
