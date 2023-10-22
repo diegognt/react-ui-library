390d91627b9c6180741ad6a365aa2c77663af9ab
Diego Navarro <code@diegognt.dev>
2023-10-22 07:48:19 -0500

    feat: adds vitest for component testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: "happy-dom"
+    environment: "happy-dom",
+    setupFiles: ["./test/setUp.ts"]
   },
+
 })
