e489623d703820a21efb5859791725a52c4cf870
Diego Navarro <code@diegognt.dev>
2023-10-22 08:32:14 -0500

    feat: adds the husky package

--- src/components/Button/index.test.tsx
+++ src/components/Button/index.test.tsx
@@ -7,14 +7,8 @@ describe("The Button component", () => {
 
   afterEach(() => cleanup())
 
-  test("throws and error if the 'label' prop is not defined", () => {
-    const act = () => render(<Button label="" />)
-
-    expect(act).toThrowError("Button must have a label")
-  })
-
   test("renders a button with a label", () => {
-    render(<Button label="Click me" />)
+    render(<Button>Click me</Button>)
 
     const button = screen.getByRole("button", { name: "Click me" })
 
@@ -22,7 +16,7 @@ describe("The Button component", () => {
   })
 
   test("by default, renders a Button with a 'primary' color, 'solid' variant, and 'medium' size", () => {
-    render(<Button label="Click me" />)
+    render(<Button>Click me</Button>)
 
     const button = screen.getByRole("button", { name: "Click me" })
 
@@ -32,7 +26,7 @@ describe("The Button component", () => {
   })
 
   test("renders a button with a 'secondary' color", () => {
-    render(<Button label="Click me" color="secondary" />)
+    render(<Button color="secondary">Click me</Button>)
 
     const button = screen.getByRole("button", { name: "Click me" })
 
@@ -40,7 +34,7 @@ describe("The Button component", () => {
   })
 
   test("renders a button with a 'ghost' variant", () => {
-    render(<Button label="Click me" variant="ghost" />)
+    render(<Button variant="ghost">Click me</Button>)
 
     const button = screen.getByRole("button", { name: "Click me" })
 
@@ -48,7 +42,7 @@ describe("The Button component", () => {
   })
 
   test("renders a button with a 'outline' variant", () => {
-    render(<Button label="Click me" variant="outline" />)
+    render(<Button variant="outline">Click me</Button>)
 
     const button = screen.getByRole("button", { name: "Click me" })
 
@@ -56,7 +50,7 @@ describe("The Button component", () => {
   })
 
   test("renders a button with a 'small' size", () => {
-    render(<Button label="Click me" size="small" />)
+    render(<Button size="small">Click me</Button>)
 
     const button = screen.getByRole("button", { name: "Click me" })
 
@@ -64,7 +58,7 @@ describe("The Button component", () => {
   })
 
   test("renders a button with a 'large' size", () => {
-    render(<Button label="Click me" size="large" />)
+    render(<Button size="large">Click me</Button>)
 
     const button = screen.getByRole("button", { name: "Click me" })
 
@@ -72,7 +66,7 @@ describe("The Button component", () => {
   })
 
   test("renders a button with a 'full-width' size", () => {
-    render(<Button label="Click me" size="full-width" />)
+    render(<Button size="full-width">Click me</Button>)
 
     const button = screen.getByRole("button", { name: "Click me" })
 
@@ -83,7 +77,7 @@ describe("The Button component", () => {
     const handleClick = vi.fn()
     const user = userEvent.setup()
 
-    render(<Button label="Click me" onClick={handleClick} />)
+    render(<Button onClick={handleClick}>Click me</Button>)
 
     const button = screen.getByRole("button", { name: "Click me" })
     await user.click(button)
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,23 +2,23 @@ import React from 'react';
 import './styles.css';
 
 export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  label: string
+  children: string
   size?: "small" | "medium" | "large" | "full-width"
   color?: "primary" | "secondary"
   variant?: "solid" | "ghost" | "outline"
 }
 
 const Button = (props: ButtonProps): JSX.Element => {
-  const { label, size = "medium", color = "primary", variant = "solid", ...rest } = props
+  const { children, size = "medium", color = "primary", variant = "solid", ...rest } = props
 
-  if (!label) throw new Error("Button must have a label")
+  if (!children) throw new Error("Button must have children")
 
   return (
     <button
       className={`button button--${size} button--${color} button--${variant}`}
       {...rest}
     >
-      {label}
+      {children}
     </button>
   )
 }
