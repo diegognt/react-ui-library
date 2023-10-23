2312429c550d26667b4e627077c855c2745b96b4
Diego Navarro <code@diegognt.dev>
2023-10-23 04:58:41 -0500

    feat: adds Stories dor the Heading component with styles

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,13 +1,15 @@
 import React from "react"
 
-type HeaderProps = React.HTMLAttributes<HTMLHeadingElement> & {
+import "./styles.css"
+
+export type HeaderProps = React.HTMLAttributes<HTMLHeadingElement> & {
   children: string,
   as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6",
-  size?: "xs" | "sm" | "md" | "lg" | "xlg" | "2xlg" | "3xlg",
+  size?: "sm" | "md" | "lg" | "xl" | "2xl" | "3xl",
 }
 
 const Heading = (props: HeaderProps): JSX.Element => {
-  const { children, as = "h1", size = "md", ...rest } = props
+  const { children, as = "h1", size = "3xl", ...rest } = props
 
   if (!children) throw new Error("Heading must have children")
 
