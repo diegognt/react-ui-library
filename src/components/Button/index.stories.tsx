b3b33718d5f1a1c6dc0b51aeea2158bc45511eec
Diego Navarro <code@diegognt.dev>
2023-10-23 04:57:53 -0500

    fix: fixes Storybook story regarding the "label" prop

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -15,46 +15,38 @@ export default meta
 
 type Story = StoryObj<typeof Button>
 
-
-
 const sizes = ['small', 'medium', 'large'] as const;
 const colors = ['primary', 'secondary'] as const;
 const variants = ['solid', 'ghost', 'outline'] as const;
 
 export const Sizes: Story = {
-  args: {
-    label: 'Button',
-  },
+  args: {},
   render: (args: ButtonProps) => (<section style={{ alignItems: 'center', display: 'flex', gap: '2rem' }}>
     {
       sizes.map((size) => (
-        <Button {...args} size={size} key={size} />
+        <Button {...args} size={size} key={size}>Button</Button>
       ))
     }
   </ section>),
 }
 
 export const Colors: Story = {
-  args: {
-    label: 'Button',
-  },
+  args: {},
   render: (args: ButtonProps) => (<section style={{ alignItems: 'center', display: 'flex', gap: '2rem' }}>
     {
       colors.map((color) => (
-        <Button {...args} color={color} key={color} />
+        <Button {...args} color={color} key={color}>A Button</Button>
       ))
     }
   </ section>),
 }
 
 export const Variants: Story = {
-  args: {
-    label: 'Button',
-  },
+  args: {},
   render: (args: ButtonProps) => (<section style={{ alignItems: 'center', display: 'flex', gap: '2rem' }}>
     {
       variants.map((variant) => (
-        <Button {...args} variant={variant} key={variant} />
+        <Button {...args} variant={variant} key={variant}>A Button</Button>
       ))
     }
   </ section>),
